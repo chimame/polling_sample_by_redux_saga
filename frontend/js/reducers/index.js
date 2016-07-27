9cc60664f60327f7d4714e65ce12f9e92df37254
chimame <rito.tamata@gmail.com>
2016-07-27 13:36:39 +0900

    Fix client time does not appear

--- frontend/js/reducers/index.js
+++ frontend/js/reducers/index.js
@@ -8,7 +8,7 @@ const reducerMap = {
     return {
       show_error: false,
       server_time: action.payload.server_time,
-      clinet_time: `${today.getFullYear()}/${('0' + (today.getMonth() + 1)).slice(-2)}/${('0' + today.getDate()).slice(-2)} ${('0' + today.getHours()).slice(-2)}:${('0' + today.getMinutes()).slice(-2)}`
+      client_time: `${today.getFullYear()}/${('0' + (today.getMonth() + 1)).slice(-2)}/${('0' + today.getDate()).slice(-2)} ${('0' + today.getHours()).slice(-2)}:${('0' + today.getMinutes()).slice(-2)}:${('0' + today.getSeconds()).slice(-2)}`
     }
   },
   SHOW_NETWORK_ERROR(state, action) {
